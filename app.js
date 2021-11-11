22e9b4b79443eb4f691b8687a2fa1014cb112157
galaxine <oezdemir50@gmail.com>
2021-11-11 16:09:21 +0100

    Need to revert to windows 10 due to shenanigans EDIT: I was tired as hell and made some bad things that do not belong outside

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 let library = [];
 /**
  * The book class created the old-school style. For learning purposes
- * @param {String} name
- * @param {String} author 
- * @param {Number} pages 
- * @param {Boolean} read already set to false
+ * @param {String} name Name of the book
+ * @param {String} author Name of the author
+ * @param {Number} pages Pages
+ * @param {Boolean} read already set to false, 
  */
 function Book(name, author, pages, read = false) {
     this.name = name;
@@ -21,9 +21,72 @@ function Book(name, author, pages, read = false) {
 
 
 let hobbits = new Book("The hobbits", "J.K.Cowlin", 328);
+let sexasutra = new Book("Sexasutra", "Nigga", 334);
+let fuckmeatCatalogue = new Book("The Encyclopedia of females selected and engineered for sexual and breeding purposes", "the science team", 328)
+
 /**
- * Adding the books to the library
+ * Add books to the library through a function
  */
-function addBooksToLibrary(name, author, pages) {
-    library.push(new Book(name, author, pages));
+function addBooksToLibrary(name, author, pages, read) {
+    if (read == undefined) {
+        library.push(new Book(name, author, pages));
+    } else {
+        library.push(new Book(name, author, pages, read));
+    }
 }
+
+library.push(hobbits);
+library.push(sexasutra);
+library.push(fuckmeatCatalogue);
+
+
+function loopThroughLibrary() {
+    library.forEach(book => {
+        book.info();
+    });
+}
+console.log(loopThroughLibrary())
+
+
+/* Modal part */
+const openModalButtons = document.querySelectorAll("[data-modal-target]")
+const closeModalButtons = document.querySelectorAll("[data-close-button]")
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+/*
+Getting the results of the modal.
+*/
+
+const resultList = document.getElementById('results');
